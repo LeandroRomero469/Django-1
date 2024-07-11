d77aeae0771d6f8b7a2c930d91c6aea51b54c5f1
Romero Leandro <leandroromero186@gmail.com>
2024-07-11 20:52:03 -0300

    Modificamos views.py, index,html, urls.py

--- Proyecto1/views.py
+++ Proyecto1/views.py
@@ -7,7 +7,7 @@ from django.template import Template, Context
 def saludo(request):
     return HttpResponse("Hola mundo!, hola Coder!")
 
-def mostrar_nombres(request):
+def mostrar_nombre(request):
     texto = "Soy Leandro Romero<br>Cursando Python"
     return HttpResponse(texto)
 
@@ -18,17 +18,21 @@ def dia_de_hoy(request):
 
 def probando_template(request):
 
-    # Abrimos el archivo html
+    nombre = "Adrian"
+    apellido = "Holovaty"
+    diccionario = {"nombre": nombre, "apellido": apellido, "notas": [4, 8, 9, 10, 7, 8]}
+
+# Abrimos el archivo html
     mi_html = open('./Proyecto1/plantillas/index.html')
 
     # Creamos el template haciendo uso de la clase Template
     plantilla = Template(mi_html.read())
 
-    # Cerramos el archivo previamente abierto, ya que lo tenemos cargado en la variable plantilla
+# Cerramos el archivo previamente abierto, ya que lo tenemos cargado en la variable plantilla
     mi_html.close()
 
-    # Creamos un contexto, más adelante vamos a aprender a usarlo, ahora lo necesitamos aunque sea vacío para que funcione
-    mi_contexto = Context()
+# Creamos un contexto con los datos del diccionario
+    mi_contexto = Context(diccionario)
 
     # Terminamos de construír el template renderizándolo con su contexto
     documento = plantilla.render(mi_contexto)
